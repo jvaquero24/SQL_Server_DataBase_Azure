ad68c371ebe7a5c918feeca4e635860ada3fd015
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-10-16 11:53:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
-data "azurerm_resource_group" "RG" {
-  name = var.resource_group_name
-}
-
 resource "azurerm_sql_server" "SQLSERVER" {
   name                         = var.sqlserver_name
   location                     = var.location
-  resource_group_name          = data.azurerm_resource_group.RG.name
+  resource_group_name          = var.resource_group_name_dbs
   version                      = "12.0"
   administrator_login          = var.admin_login
   administrator_login_password = var.admin_pass
@@ -15,6 +11,6 @@ resource "azurerm_sql_server" "SQLSERVER" {
 resource "azurerm_sql_database" "SQLDATABASE" {
   name                = var.sqldatabase_name
   location            = var.location
-  resource_group_name = data.azurerm_resource_group.RG.name
+  resource_group_name = var.resource_group_name_dbs
   server_name         = var.serversqlname
 }
