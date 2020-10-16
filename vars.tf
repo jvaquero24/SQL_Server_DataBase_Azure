f4850e25beecfdcbbce40d1d41e08e7d62d940f8
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-10-16 02:45:09 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,33 @@
+variable "resource_group_name" {
+  type        = string
+}
+variable "location" {
+  type        = string
+}
+
+
+
+variable "name_mysqlserver" {
+  type        = string
+}
+variable "administrator_login" {
+  type        = string
+}
+variable "administrator_login_password" {
+  type        = string
+}
+variable "sku_name" {
+  type        = string
+}
+variable "storage_mb" {
+  type        = string
+}
+variable "version" {
+  type        = string
+}
+variable "auto_grow_enabled" {
+  type        = string
+}
+variable "name_sqldatabase" {
+  type        = string
+}
